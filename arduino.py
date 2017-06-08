4c956ff83a3d8fd78c929f9cfd234cf70bf1c604
cactus-code <cactus-code@users.noreply.github.com>
2017-06-09 10:22:56 +1200

    Started class

--- arduino.py
+++ arduino.py
@@ -1,1 +1,15 @@
-
+import itertools
+import logging
+import serial
+import time
+import platform
+from serial.tools import list_ports
+if platform.system() == 'Windows':
+    import _winreg as winreg
+else:
+    import glob
+    
+log = logging.getLogger(__name__)
+    
+class Arduino(object):
+  def getPort():
