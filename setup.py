743e64e644ad0aecce3d102e75be120e41cdb622
cactus-code <cactus-code@users.noreply.github.com>
2017-06-09 13:24:07 +1200

    Added data
    Missing archive - need to update

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyduino',
+  packages = ['pyduino'],
+  version = '0.1',
+  description = 'A lib for interaction with Arduino in Python 3',
+  author = 'cactus-code',
+  author_email = 'cactuscodenz@gmail.com',
+  url = 'https://github.com/cactus-code/pyduino',
+  download_url = 'https://github.com/cactus-code/pyduino/archive/0.1.tar.gz',
+  keywords = ['arduino','python3.5.2'],
+  classifiers = [],
+)
