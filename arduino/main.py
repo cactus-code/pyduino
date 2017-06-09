8fdb440a6adf7d567ce8040e11721380376c0892
cactus-code <tntwizardx@gmail.com>
2017-06-09 22:49:34 +1200

    Added function

--- arduino/main.py
+++ arduino/main.py
@@ -11,7 +11,24 @@ else:
     
 log = logging.getLogger(__name__)
 
-def get_ports(
+def get_windows_ports():
+    """
+    Gets the list of possible COM ports on the computer.
+    Uses winreg module.
+    """
+
+def build_cmd_str(cmd, args=None):
+    """
+    Builds a command string that can be sent to the Arduino.
+    """
+    if args:
+        args = '%'.join(map(str, args))
+    else:
+        args = ''
+    return "@{cmd}%{args}$!".format(cmd, args)
+
+def find_port(baud, timeout):
+    
     
 class Arduino(object):
     def __init__(self, baud=9600, port=None, timeout=2, sr=None):
